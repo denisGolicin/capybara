3b756baa46275903f793c4719ac62326f375fa9a
DGDeveloper <111699872+denisGolicin@users.noreply.github.com>
2023-01-21 16:34:31 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -187,6 +187,81 @@ for(let i = 0; i < buttonsYouGet.length; i++){
         console.log("click" + i)
     })
 }
+
+a = document.querySelector('.block-youGet');
+a.addEventListener('touchstart', handleTouchStart, false);        
+a.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+return evt.touches ||             // browser API
+        evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                        
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                        
+function handleTouchMove(evt) {
+    if(!isMobile) return;
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                        
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* right swipe */ 
+            //alert('right')
+            
+            if(buttonid >= buttonsYouGet.length -1) return;
+            let last = buttonid;
+            buttonid += 1;
+            
+            
+
+            changeBlockYouGet(buttonid, last);
+            buttonsYouGet[buttonid].classList.add('youGet-button-active');
+            buttonActive.classList.remove('youGet-button-active');
+
+            buttonActive = buttonsYouGet[buttonid];
+        } else {
+            /* left swipe */
+            if(buttonid <= 0) return;
+            let last = buttonid;
+            buttonid -= 1;
+            
+
+            changeBlockYouGet(buttonid, last);
+            buttonsYouGet[buttonid].classList.add('youGet-button-active');
+            buttonActive.classList.remove('youGet-button-active');
+
+            buttonActive = buttonsYouGet[buttonid];
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* down swipe */ 
+        } else { 
+            /* up swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
+
+
+
 let weDoItem = [];
 weDoItem = document.querySelectorAll('.block-weDo-item');
 document.body.addEventListener("scroll", function () {
